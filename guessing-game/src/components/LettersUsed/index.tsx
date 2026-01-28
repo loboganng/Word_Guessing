9012debd1179d7507aea9612e900f9d5060eb36e
Wolfgang Neves <wolfgang.neves@gmail.com>
2026-01-28 16:01:37 -0300

    feat: Creating variation of letter sizes for guessed letters, and also color patterns for correct and wrong guessed letters

--- guessing-game/src/components/LettersUsed/index.tsx
+++ guessing-game/src/components/LettersUsed/index.tsx
@@ -1,8 +1,15 @@
 import styles from "./styles.module.css"
 
+import { Letter } from "../Letter"
+
 export function LettersUsed(){
   return (
   <div className={styles.lettersUsed}>
     <h5>Letters used</h5>
+
+    <div>
+      <Letter value="r" size="small" color="correct"/>
+      <Letter value="x" size="small" color="wrong"/>
+    </div>
   </div>)
 }
