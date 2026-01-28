36d3ec056302ad07916e63bcfea8d0d02547bf92
Wolfgang Neves <wolfgang.neves@gmail.com>
2026-01-28 15:09:53 -0300

    feat: Creating and styling input element

--- guessing-game/src/App.tsx
+++ guessing-game/src/App.tsx
@@ -3,6 +3,7 @@ import styles from "./app.module.css"
 import { Header } from "./components/Header"
 import { Tip } from "./components/Tip"
 import { Letter } from "./components/Letter"
+import { Input } from "./components/Input"
 
 export default function App(){
 
@@ -22,6 +23,12 @@ export default function App(){
           <Letter value="A"/>
           <Letter value="C"/>
           <Letter value="T"/>
+        </div>
+
+        <h4>Guesses</h4>
+
+        <div>
+          <Input autoFocus maxLength={1} placeholder="?"/>
         </div>
       </main>
     </div>
