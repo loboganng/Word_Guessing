dfd1d3fd1e0e2ebe807ff32564b62ac6b3bbf9b2
Wolfgang Neves <wolfgang.neves@gmail.com>
2026-01-29 08:21:57 -0300

    feat: State to create initial chosen word from the available words list

--- guessing-game/src/App.tsx
+++ guessing-game/src/App.tsx
@@ -1,4 +1,8 @@
 import styles from "./app.module.css"
+import { useEffect, useState } from "react"
+
+import { WORDS, Challenge } from "./utils/words"
+
 
 import { LettersUsed } from "./components/LettersUsed"
 import { Header } from "./components/Header"
@@ -8,15 +12,33 @@ import { Input } from "./components/Input"
 import { Tip } from "./components/Tip"
 
 export default function App(){
+  //Creating initial useStates
+  const [attempts, setAttempts] = useState(0)
+  const[letter, setLetter] = useState("")
+  const [challenge, setChallenge] = useState<Challenge | null>(null)
 
   function handleRestartGame(){
     alert("Restart game!")
   }
 
+  function startGame(){
+    const index = Math.floor(Math.random() * WORDS.length)
+    const randomWord = WORDS[index]
+
+    setChallenge(randomWord)
+
+    setAttempts(0)
+    setLetter("")
+  }
+
+  useEffect(() => {
+    startGame()
+  }, [])
+
   return(
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame}/>
+        <Header current={attempts} max={10} onRestart={handleRestartGame}/>
         
         <Tip tip="Dynamic programming language"/>
         <div className={styles.word}>
